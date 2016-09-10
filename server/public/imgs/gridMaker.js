043c3c62613e07d2f177e6828299be01eb3917b8
twinklefingers <rachelweila@gmail.com>
2016-09-10 18:57:46 -0500

    capturing unique ids and bg color of divs

--- server/public/imgs/gridMaker.js
+++ server/public/imgs/gridMaker.js
@@ -1,145 +1,145 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var canvasModButton = document.getElementById('canvasModButton');
-    var pixelContainer = document.getElementById('pixelContainer');
-    var resetButton = document.getElementById('resetButton');
-    var mousingDown = false;
-    //
-    // window.addEventListener('mousedown', switchMouseState);
-    // window.addEventListener('mouseup', switchMouseState);
-    // canvasModButton.addEventListener('click', openCanvasModal);
-    // pixelContainer.addEventListener('mouseover', setPixelColor);
-    // pixelContainer.addEventListener('click', setPixelColor);
-    // resetButton.addEventListener('click', resetPixels);
-    //
-    // //Updates mousingDown to reflect whether the mouse is being clicked
-    //Takes the event object as a parameter
-    function switchMouseState(event) {
-        mousingDown = event.type === 'mousedown';
-    }
-
-    //Brings up the canvas Modal and assigns event listeners to its elements
-    function openCanvasModal() {
-        var modal = document.getElementById('sizeChangeModal');
-
-        modal.style.display = 'block';
+        var canvasModButton = document.getElementById('canvasModButton');
+        var pixelContainer = document.getElementById('pixelContainer');
+        var resetButton = document.getElementById('resetButton');
+        var mousingDown = false;
         //
-        // var closeButton = document.getElementById('close');
-        // var pixelModButton = document.getElementById('pixelModButton');
-        var inputs = document.getElementsByTagName('input');
-
-        var specInputs = {
-            'percentOfScreenInput': document.getElementById('percentOfScreenInput'),
-            'colsInput': document.getElementById('colsInput')
-        };
-
-        var atLeastOneBlankInput = true;
-
-        for (i = 0; i < inputs.length; i++) {
-            inputs[i].onkeydown = setInputToNumbers(atLeastOneBlankInput, specInputs, pixelModButton);
+        // window.addEventListener('mousedown', switchMouseState);
+        // window.addEventListener('mouseup', switchMouseState);
+        // canvasModButton.addEventListener('click', openCanvasModal);
+        // pixelContainer.addEventListener('mouseover', setPixelColor);
+        // pixelContainer.addEventListener('click', setPixelColor);
+        // resetButton.addEventListener('click', resetPixels);
+        //
+        // //Updates mousingDown to reflect whether the mouse is being clicked
+        //Takes the event object as a parameter
+        function switchMouseState(event) {
+            mousingDown = event.type === 'mousedown';
         }
 
-        closeButton.onclick = closeModal(modal);
-        pixelModButton.onclick = setPixelSpecs(specInputs, modal);
-    }
+        //Brings up the canvas Modal and assigns event listeners to its elements
+        function openCanvasModal() {
+            var modal = document.getElementById('sizeChangeModal');
 
-    function closeModal(modal) {
-        return function(event) {
-            modal.style.display = 'none';
-        }
-    }
+            modal.style.display = 'block';
+            //
+            // var closeButton = document.getElementById('close');
+            // var pixelModButton = document.getElementById('pixelModButton');
+            var inputs = document.getElementsByTagName('input');
 
-    //
-    function setInputToNumbers(atLeastOneBlankInput, specInputs, pixelModButton) {
-        return function(event) {
-            event.preventDefault();
-            if (event.keyCode >= 48 && event.keyCode <= 57) {
-                event.target.value += event.key;
-            } else if (event.keyCode === 8) {
-                event.target.value = event.target.value.slice(0, -1);
+            var specInputs = {
+                'percentOfScreenInput': document.getElementById('percentOfScreenInput'),
+                'colsInput': document.getElementById('colsInput')
+            };
+
+            var atLeastOneBlankInput = true;
+
+            for (i = 0; i < inputs.length; i++) {
+                inputs[i].onkeydown = setInputToNumbers(atLeastOneBlankInput, specInputs, pixelModButton);
             }
-            if (atLeastOneBlankInput || event.keyCode === 8) {
-                atLeastOneBlankInput = false;
-                for (var spec in specInputs) {
-                    var currentTextInput = specInputs[spec].value;
-                    var currentNumInput = parseInt(currentTextInput);
-                    if (currentTextInput === '' || currentNumInput < 1 || currentNumInput > 100) {
-                        atLeastOneBlankInput = true;
+
+            closeButton.onclick = closeModal(modal);
+            pixelModButton.onclick = setPixelSpecs(specInputs, modal);
+        }
+
+        function closeModal(modal) {
+            return function(event) {
+                modal.style.display = 'none';
+            }
+        }
+
+        //
+        function setInputToNumbers(atLeastOneBlankInput, specInputs, pixelModButton) {
+            return function(event) {
+                event.preventDefault();
+                if (event.keyCode >= 48 && event.keyCode <= 57) {
+                    event.target.value += event.key;
+                } else if (event.keyCode === 8) {
+                    event.target.value = event.target.value.slice(0, -1);
+                }
+                if (atLeastOneBlankInput || event.keyCode === 8) {
+                    atLeastOneBlankInput = false;
+                    for (var spec in specInputs) {
+                        var currentTextInput = specInputs[spec].value;
+                        var currentNumInput = parseInt(currentTextInput);
+                        if (currentTextInput === '' || currentNumInput < 1 || currentNumInput > 100) {
+                            atLeastOneBlankInput = true;
+                        }
+                    }
+                    pixelModButton.disabled = atLeastOneBlankInput;
+                }
+            }
+        }
+
+        //Creates a new canvas with the specifications given by the user
+        //Takes the % of screen and # columns
+        //Puts an error message at the bottom of the modal if not all of the specs are
+        //whole numbers
+        function setPixelSpecs(specInputs, modal) {
+            return function(event) {
+                var percentOfScreenInput = specInputs['percentOfScreenInput'].value;
+                var numCols = specInputs['colsInput'].value;
+                var pixelWidthAndHeight = 100 / numCols;
+                var numPixelsDesired = Math.pow(numCols, 2);
+                var numPixelsCurrently = pixelContainer.childElementCount;
+                var pixelDifference = numPixelsDesired - numPixelsCurrently;
+                var numPixelsToAdjust = Math.abs(pixelDifference);
+
+                for (i = 0; i < numPixelsToAdjust; i++) {
+                    if (pixelDifference > 0) {
+                        var pixel = document.createElement('div');
+                        pixel.className = 'pixel';
+                        pixel.style.width = pixelWidthAndHeight + '%';
+                        pixel.style.paddingBottom = pixelWidthAndHeight + '%';
+                        pixel.style.borderTop = 'none';
+                        if ((i + numPixelsCurrently + 1) % numCols === 0) {
+                            pixel.style.borderRight = 'solid 1px';
+                        } else {
+                            pixel.style.borderRight = 'none';
+                        }
+                        pixelContainer.appendChild(pixel);
+                    } else if (pixelDifference < 0) {
+                        var lastChild = pixelContainer.lastChild;
+                        lastChild.removeEventListener('mouseover', function() {});
+                        pixelContainer.removeChild(pixelContainer.lastChild);
                     }
                 }
-                pixelModButton.disabled = atLeastOneBlankInput;
-            }
-        }
-    }
 
-    //Creates a new canvas with the specifications given by the user
-    //Takes the % of screen and # columns
-    //Puts an error message at the bottom of the modal if not all of the specs are
-    //whole numbers
-    function setPixelSpecs(specInputs, modal) {
-        return function(event) {
-            var percentOfScreenInput = specInputs['percentOfScreenInput'].value;
-            var numCols = specInputs['colsInput'].value;
-            var pixelWidthAndHeight = 100 / numCols;
-            var numPixelsDesired = Math.pow(numCols, 2);
-            var numPixelsCurrently = pixelContainer.childElementCount;
-            var pixelDifference = numPixelsDesired - numPixelsCurrently;
-            var numPixelsToAdjust = Math.abs(pixelDifference);
-
-            for (i = 0; i < numPixelsToAdjust; i++) {
-                if (pixelDifference > 0) {
-                    var pixel = document.createElement('div');
-                    pixel.className = 'pixel';
-                    pixel.style.width = pixelWidthAndHeight + '%';
-                    pixel.style.paddingBottom = pixelWidthAndHeight + '%';
-                    pixel.style.borderTop = 'none';
-                    if ((i + numPixelsCurrently + 1) % numCols === 0) {
-                        pixel.style.borderRight = 'solid 1px';
+                var pixels = pixelContainer.children;
+                for (i = 0; i < pixels.length - numPixelsToAdjust; i++) {
+                    pixels[i].className = 'pixel';
+                    if (i <= numCols - 1) {
+                        pixels[i].style.borderTop = 'solid 1px';
                     } else {
-                        pixel.style.borderRight = 'none';
+                        pixels[i].style.borderTop = 'none';
                     }
-                    pixelContainer.appendChild(pixel);
-                } else if (pixelDifference < 0) {
-                    var lastChild = pixelContainer.lastChild;
-                    lastChild.removeEventListener('mouseover', function() {});
-                    pixelContainer.removeChild(pixelContainer.lastChild);
+                    if ((i + 1) % numCols === 0) {
+                        pixels[i].style.borderRight = 'solid 1px';
+                    }
+                    pixels[i].style.width = pixelWidthAndHeight + '%';
+                    pixels[i].style.paddingBottom = pixelWidthAndHeight + '%';
                 }
-            }
 
+                pixelContainer.style.width = percentOfScreenInput + '%';
+
+                modal.style.display = 'none';
+            }
+        }
+
+        //Sets the pixel color of the pixel over which the mouse is dragging
+        function setPixelColor(event) {
+            if (event.type === 'click') {
+                event.target.style.backgroundColor = 'red';
+            } else if (mousingDown) {
+                event.target.style.backgroundColor = 'red';
+            }
+        }
+
+        //Resets all pixels to be blank
+        function resetPixels(event) {
             var pixels = pixelContainer.children;
-            for (i = 0; i < pixels.length - numPixelsToAdjust; i++) {
-                pixels[i].className = 'pixel';
-                if (i <= numCols - 1) {
-                    pixels[i].style.borderTop = 'solid 1px';
-                } else {
-                    pixels[i].style.borderTop = 'none';
-                }
-                if ((i + 1) % numCols === 0) {
-                    pixels[i].style.borderRight = 'solid 1px';
-                }
-                pixels[i].style.width = pixelWidthAndHeight + '%';
-                pixels[i].style.paddingBottom = pixelWidthAndHeight + '%';
+            for (i = 0; i < pixels.length; i++) {
+                pixels[i].style.backgroundColor = '';
             }
-
-            pixelContainer.style.width = percentOfScreenInput + '%';
-
-            modal.style.display = 'none';
         }
-    }
-
-    //Sets the pixel color of the pixel over which the mouse is dragging
-    function setPixelColor(event) {
-        if (event.type === 'click') {
-            event.target.style.backgroundColor = 'red';
-        } else if (mousingDown) {
-            event.target.style.backgroundColor = 'red';
-        }
-    }
-
-    //Resets all pixels to be blank
-    function resetPixels(event) {
-        var pixels = pixelContainer.children;
-        for (i = 0; i < pixels.length; i++) {
-            pixels[i].style.backgroundColor = '';
-        }
-    }
-})
+    }) // end doc listener
--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,14 +1,87 @@
 $(document).ready(function() {
-    console.log('jQuery linked');
+    console.log('jQuery linked'); // this is really important! :P
 
     // getData();
     $('#chooseGrid').hide();
     // //button listeners
     $('.click4grid').on('click', toggleOptions);
+    // $('#gridCanvas').children().on('click', logPixel);
+
+
+    // $('#gridCanvas').children().click(function() {
+    //     var $id = $(this).attr('id');
+    //     console.log($id);
+
+    // });
+
+
+
     // // $('#submitTestData').on("click", postData);
     // $('#dataTable').on("click", ".delete", deleteData);
     // $('#dataTable').on("click", ".update", updateData);
 }); // end doc ready
+
+function colorGrid(color) {
+    // var color = $('#color').attr('class');
+
+    // check if a color has been selected, if not then black
+    // else if color has been selected, use that color
+    if (color == 'black') {
+        console.log("color black was selected");
+        pickedColor = "black";
+        console.log("new pickedColor: ", pickedColor);
+    } else
+    if (color == "red") {
+        console.log("color red selected");
+        pickedColor = 'red';
+        console.log("new pickedColor: ", pickedColor);
+    } else if (color == "white") {
+        console.log("color white selected");
+        pickedColor = "white";
+        console.log("new pickedColor: ", pickedColor);
+    }
+    return pickedColor;
+}
+
+
+function logPixel(pixel) {
+    // var id = $(this).attr('id');
+    var color = $('.pixel').css('backgroundColor');
+
+    var id = pixel.id;
+    // var color = pixel.style.backgroundColor;
+    console.log("color: ", color);
+    console.log("pixel id: ", id);
+    drawColor(color);
+
+    function drawColor(color) {
+        console.log("drawColor() ready for action");
+
+        // var white = 'rgb(255, 255, 255)';
+        // var red = 'rgb(255, 0, 0)';
+        // var black = 'rgb(0, 0, 0)';
+        // var pickedcolor = '';
+        // var color = $('#color').attr('class');
+        console.log("pickedColor in drawColor: ", pickedColor);
+
+        // check if a color has been selected, if not then black
+        // else if color has been selected, use that color
+        if (pickedColor == color) {
+            console.log("the color has already been written");
+        } else
+        if (color == "red") {
+            console.log("color red selected");
+        } else if (color == "white") {
+            console.log("color white selected");
+        }
+
+        // check if they are pressing down on the mouse..
+        // ..if true, then color background div w/ unique id
+
+    }
+
+}
+
 
 // shows or hides form to gather grid info
 function toggleOptions() {
@@ -17,33 +90,36 @@ function toggleOptions() {
 
 }
 
-gridCanvas = $('#gridCanvas');
-
-
-
-
-// var Grid = {
-//     sts: $('#gridStitches').attr('value'),
-//     rows: rows,
-//     horizontal: horizontal,
-//     vertical: vertical
-// };
-
+var gridCanvas = $('#gridCanvas');
 
 // captures user custom grid specifications
 function submitGridSpecs() {
     console.log('submitGridSpecs() triggered');
     event.preventDefault();
+    // clear inputs
 
-    // jquery method to get value of user inputs 1
-    var sts = $('#gridStitches').val();
-    console.log("sts: ", sts); // worked!
-    var rows = $('#gridRows').val();
-    console.log("rows: ", rows);
-    var horizontal = $('#horizontalGauge').val();
-    console.log("horizontal: ", horizontal);
-    var vertical = $('#verticalGauge').val();
-    console.log("vertical: ", vertical);
+    var Grid = {
+        sts: $('#gridStitches').val(),
+        rows: $('#gridRows').val(),
+        horizontal: $('#horizontalGauge').val(),
+        vertical: $('#verticalGauge').val()
+
+        // same as:
+        // sts: $('#gridStitches').val(),
+        // rows: document.getElementById('gridRows').value,
+    };
+    console.log("Grid: ", Grid);
+
+
+    // // jquery method to get value of user inputs 1
+    // var sts = $('#gridStitches').val();
+    // console.log("sts: ", sts); // worked!
+    // var rows = $('#gridRows').val();
+    // console.log("rows: ", rows);
+    // var horizontal = $('#horizontalGauge').val();
+    // console.log("horizontal: ", horizontal);
+    // var vertical = $('#verticalGauge').val();
+    // console.log("vertical: ", vertical);
 
     // // jQuery method to get values of user input 2
     // var sts = $('#gridStitches').attr('value');
@@ -57,21 +133,38 @@ function submitGridSpecs() {
     // console.log("sts: ", sts); // worked!
 
 
-    newGrid(sts); // must be placed after var assignment
+    newGrid(Grid); // must be placed after var assignments
 }
 
 // uses captured data and appends to DOM
-function newGrid(sts) {
+function newGrid(Grid) {
     console.log('newGrid() triggered');
-    console.log("newGrid() sts: ", sts);
-    gridCanvas.innerHTML = '';
+    console.log("newGrid() sts: ", Grid.sts);
     // draw
-    for (i = 1; i <= sts; i++) {
-        $('#gridCanvas').append("<div class='pixel'>" + i + "</div>");
+    for (i = 1; i <= Grid.sts; i++) {
+        $('#gridCanvas').append("<div class='pixel' id='pixel" + i + "' onclick='logPixel(this)'>" + i + "</div>");
         console.log('div created');
     }
-
+    //compare div px to container width and see if you can set col x rows that way?
 }
+
+
+//
+// $(".pixel").mousedown(function(id) {
+//     console.log("mousedown function triggered");
+//     //color bg of selected div
+//     var $id = $(this).attr('id');
+//     console.log(this);
+//     var color = $(this).css("background-color");
+//     $id.css().background("red");
+// });
+
+
+
+
+
+
+
 
 // // function deleteData() {
 //     var testdataID = $(this).attr("id"); //this = #dataTable, .delete
